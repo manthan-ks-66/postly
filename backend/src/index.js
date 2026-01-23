01c6386cbe4188e8a4f2603f972784a9975da19c
Manthan Srivastava <manthanks.0606@gmail.com>
2026-01-23 10:55:37 +0530

    change console in index file

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ let port_no = process.env.PORT_NO || 3000;
 connect()
   .then(() => {
     app.listen(port_no, () => {
-      console.log(`Postly server is running on port no. ${port_no}`);
+      console.log(`server is running on port no. ${port_no}`);
     });
   })
   .catch((error) => console.log(error.message));
