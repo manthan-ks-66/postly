73377ea5054430340952a943b948139158120889
Manthan Srivastava <manthanks.0606@gmail.com>
2026-03-24 13:23:57 +0530

    set up cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
